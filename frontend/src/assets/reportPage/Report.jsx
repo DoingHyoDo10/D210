9dd290ce29306ee2a4cbdf9232a134e13806f9fa
qysim <qysim@naver.com>
2024-04-04 05:15:29 +0900

    feat: 운동페이지 달력 수정

--- frontend/src/assets/reportPage/Report.jsx
+++ frontend/src/assets/reportPage/Report.jsx
@@ -1,8 +1,20 @@
 import styles from "./Report.module.css"
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
+import { getReport } from "../../apis/exercise";
 
 const Report = function () {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        getReport();
+      } catch (err) {
+        console.error('리포트 조회 실패 : ', err)
+      }
+    })();
+  }, []);
 
   return(
     <div className={styles.report_container}>
@@ -10,6 +22,9 @@ const Report = function () {
         <div className={styles.report_top_bar}>
           <img className={styles.report_back_direct} src="/imgs/direct.png" alt="back_direct" onClick={() => {navigate('/main')}}/>
           <span className={styles.report_title}>운동 리포트</span>
+        </div>
+        <div className={styles.report_sub_container}>
+
         </div>
       </>
     </div>

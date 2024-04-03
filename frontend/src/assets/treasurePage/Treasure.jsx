361e28d34677ccc68b03aae47425c8fd3348f59a
sehyun <germs1020@gmail.com>
2024-04-04 06:29:39 +0900

    feat: 보물찾기 초안 추가

--- frontend/src/assets/treasurePage/Treasure.jsx
+++ frontend/src/assets/treasurePage/Treasure.jsx
@@ -1,6 +1,8 @@
 import styles from "./Treasure.module.css"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getMonthlyExerciseData } from "../../apis/exercise";
+import { getEggMoney,  } from "../../apis/wallet";
 
 
 const Treasure = function(){
@@ -9,6 +11,42 @@ const Treasure = function(){
     const moveToMainPage = function () {
         navigate("/main")
     }
+    useEffect(()=>{
+      getMonthlyExerciseData(JSON.parse(localStorage.getItem('tokens')).member_id)
+        .then(r=>{
+          let totalSteps = 0;
+          r.forEach(d=>{
+            totalSteps += d.steps;
+          })
+          let pos = 0;
+          const arr = [10000,50000,100000,200000,300000,400000]
+          for(let i=0; i<arr.length; i++){
+            if(totalSteps<arr[i]){
+              pos = i;
+              setPos(position[i]);
+              break;
+            }
+          }
+
+        });
+        getEggMoney()
+          .then(res=>setEgg(res.egg))
+    }, [])
+
+
+    const [profileUrl, setProfileUrl] = useState(JSON.parse(localStorage.getItem('tokens')).member_profile_url);
+    const [nickname, setNickname] = useState(JSON.parse(localStorage.getItem('tokens')).nickname);
+    const [pos, setPos] = useState({});
+    const [egg, setEgg] = useState(0);
+
+    const position = [
+      {x:'24%', y:'13%'}, 
+      {x:'67.5%', y:'26%'}, 
+      {x:'18%', y:'37%'},
+      {x:'65%', y:'53%'},
+      {x:'28%', y:'68%'},
+      {x:'65%', y:'80%'},
+    ]
 
     return(
         <>
@@ -18,7 +56,13 @@ const Treasure = function(){
                     <p className={styles.title_txt}>보물찾기</p>
                 </div>
                 <div className={styles.map_container}>
-                    <img src="/imgs/beach.jpg" alt="해변가" className={styles.map_img}></img>
+                <div style={{width:'100px', height:'50px', position:'absolute', background: 'orange', left:'1rem', top:'5rem', textAlign:'center'}}>보유 에그 {egg}</div>
+                  <div className={styles.marker_container} style={{left: `${pos.x}`, bottom: `${pos.y}`, backgroundImage: 'url(/imgs/yes_marker.png)'}}>
+                    <div className={styles.marker_inside}>
+                      <img src={profileUrl}/>
+                    </div>
+                  </div>
+                    <img src="/imgs/treasure_map.png" alt="해변가" className={styles.map_img}></img>
                 </div>
             </div>
         </>
